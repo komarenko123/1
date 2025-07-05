c7a0127e4ed7bb02387fb87d7c82d9f44edde872
komarenko123 <a0097689a@gmail.com>
2025-07-05 17:17:45 +0300

    бновление сборки: 05.07.2025 17:17

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/1/',  // Указано имя вашего репозитория "1"
   server: {
     proxy: {
       '/api': {
